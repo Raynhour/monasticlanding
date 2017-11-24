685575b527532f2833fa2e34127199aa2d7dd84a
Raynhour <waqdet@gmail.com>
2017-11-24 18:45:56 +0200

    final version

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,52 @@
 $('document').ready(function(){
+  $('#theses-nav').click(function(){
+        $('html, body').animate({
+        scrollTop: $("#theses").offset().top
+    }, 1000);
+  })
+  $('#speakers-nav').click(function(){
+    $('html, body').animate({
+    scrollTop: $("#speakers").offset().top
+}, 1000);
+})
+  $('#getstarted-nav').click(function(){
+    $('html, body').animate({
+    scrollTop: $("#getstarted").offset().top
+  }, 1000);
+  })
+  $('#contacts-nav').click(function(){
+    $('html, body').animate({
+    scrollTop: $("#footer").offset().top
+  }, 1000);
+  })
+  $('#topsend-btn, #bottomsend-btn').click(function(){
+    $('.modal').css('display', 'flex');
+    return false;
+  });
+  $('#send_end').click(function(){
+    $('.modal').css('display', 'none');
+    return false;
+  });
 
+  $('.send-btn').click(function(e){
+    e.preventDefault();
 
-	
+    var form = $('.send_form');
+    // console.log(form.attr('method'));
+
+    $.ajax({
+      method: "POST",
+      url: form.attr('action'),
+      data: form.serialize(),
+      success: function(data) {
+        // console.log(data);
+        $('.modal').css('display', 'flex');
+      },
+      error: function(e){
+        $('.modal__body').text('Error');
+        $('.modal').css('display', 'flex');
+      }
+    });
+  });
+
 });
---
+++ app/mailto.php
@@ -0,0 +1,30 @@
+<?php
+
+
+if( isset($_POST['phone']) ){
+
+  $phone = $_POST['phone'];
+  
+  if ( isset($_POST['name']) ) {
+    $name = $_POST['name'];
+  } else {
+    $name = 'Undefind user';
+  }
+
+    $to = 'evgeniymeo@gofriends.pro,info@monastic.group';
+    $subject = 'Monastic';
+    $message = '
+        <html>
+            <head>
+                <title>Monastic</title>
+            </head>
+            <body>
+                <p><strong>Имя отправителя:</strong> '. $name .'</p>
+                <p><strong>Телефон отправителя:</strong> '. $phone .'</p>
+            </body>
+        </html>';
+    $headers  = "Content-type: text/html; charset=utf-8 \r\n";
+    $headers .= "lala+\r\n";
+    mail($to, $subject, $message, $headers);
+    echo $message;
+}
